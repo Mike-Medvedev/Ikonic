529bfa7f569a391be7ccd63354ac722d200c131f
Mike Medvedev <mmedvedev20@gmail.com>
2025-03-26 15:03:52 -0400

    allow user to pick image on details page and give alert and navigate away after selecting rsvp option

--- app/(tabs)/trips/[selectedTrip]/details.tsx
+++ app/(tabs)/trips/[selectedTrip]/details.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { View, StyleSheet, Image, ScrollView, Pressable, Modal } from "react-native";
-import { Avatar, Paragraph, Text } from "react-native-paper";
+import { Avatar, Button, Paragraph, Text } from "react-native-paper";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Background from "@/ui/Background";
@@ -13,10 +13,14 @@ import Feather from "@expo/vector-icons/Feather";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import EditTripForm from "@/components/EditTripForm";
 import TripDetailsModal from "@/components/TripDetailsModal";
+import * as ImagePicker from "expo-image-picker";
+
 export default function TripDetails() {
   const { selectedTrip: selectedTripID } = useLocalSearchParams();
+  const { setTrips } = useTripContext();
   const [isOwner, setOwner] = useState<string | null>(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [image, setImage] = useState<string>("");
   const { trips } = useTripContext();
   const selectedTrip = trips.find((trip) => {
     return trip.id == selectedTripID;
@@ -41,6 +45,44 @@ export default function TripDetails() {
     iconText: { flexDirection: "row", gap: 10 },
     link: {},
   });
+  async function handleSubmit(image: string) {
+    try {
+      const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/${selectedTripID}/update-trip`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ image: image ? image : null }),
+      });
+      if (!response.ok) throw new Error("Error updating trip details");
+      const result = await response.json();
+      setTrips((trips) =>
+        trips.map((trip) =>
+          trip.id === selectedTripID
+            ? {
+                ...trip,
+                image: image.trim() ? image : trip.image,
+              }
+            : trip
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+    await handleSubmit(result.assets[0].uri);
+  };
   return (
     <Background>
       <View style={{ flex: 1, width: "100%", height: "100%" }}>
@@ -56,11 +98,14 @@ export default function TripDetails() {
           )}
           <Text style={{ fontSize: 40, marginBottom: 10 }}>{selectedTrip.title}</Text>
           <View style={{ height: "40%" }}>
-            <Image
-              source={{ uri: selectedTrip.image ?? "" }}
-              style={{ height: "100%", width: 400 }}
-              resizeMode="contain"
-            />
+            {selectedTrip.image ? (
+              <Image source={{ uri: selectedTrip.image }} style={{ height: "100%", width: 400 }} resizeMode="contain" />
+            ) : (
+              <View style={styles.container}>
+                <Button onPress={pickImage}>Pick an image from camera roll</Button>
+                {image ? <Image source={{ uri: image }} style={{ width: 200, height: 200 }} /> : null}
+              </View>
+            )}
           </View>
           <View style={{ marginTop: 20 }}>
             <View style={styles.iconText}>
--- app/(tabs)/trips/[selectedTrip]/rsvp.tsx
+++ app/(tabs)/trips/[selectedTrip]/rsvp.tsx
@@ -1,22 +1,30 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useLocalSearchParams } from "expo-router";
-import { View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { Alert, View } from "react-native";
 import { Button, Divider, Text } from "react-native-paper";
 export default function RSVP() {
   const params = useLocalSearchParams();
   const tripId = params.selectedTrip;
   async function handleRSVP(userResponse: string) {
-    const user_id = await AsyncStorage.getItem("user_id");
-    if (!user_id) throw new Error("Please sign in to RSVP");
-    const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/rsvp`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: user_id,
-      },
-      body: JSON.stringify({ trip_id: tripId, user_response: userResponse }),
-    });
-    if (!response.ok) throw new Error("Error RSVPing");
+    try {
+      const user_id = await AsyncStorage.getItem("user_id");
+      if (!user_id) throw new Error("Please sign in to RSVP");
+      const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/rsvp`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: user_id,
+        },
+        body: JSON.stringify({ trip_id: tripId, user_response: userResponse }),
+      });
+      if (!response.ok) throw new Error("Error RSVPing");
+      Alert.alert("Successfully Rsvped!");
+      window.alert("Successfully Rsvped!");
+      router.replace(`/trips/${tripId}`);
+    } catch (error) {
+      Alert.alert("Error", (error as Error).message);
+      console.error(error);
+    }
   }
   return (
     <View style={{ alignItems: "center", padding: 20 }}>
