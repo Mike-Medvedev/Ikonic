a99130f7f8f3821318097672b41fb018a0ffe5f5
Mike Medvedev <mmedvedev20@gmail.com>
2025-04-09 13:11:34 -0400

    mend
    fixed profile page prop

--- app/(tabs)/profile/index.tsx
+++ app/(tabs)/profile/index.tsx
@@ -1,7 +1,5 @@
 import ProfilePage from "@/components/ProfileComponents/ProfilePage";
-import useUser from "@/hooks/useUser";
 
 export default function Profile() {
-  const { userId } = useUser();
-  return <ProfilePage userId={userId} />;
+  return <ProfilePage />;
 }
