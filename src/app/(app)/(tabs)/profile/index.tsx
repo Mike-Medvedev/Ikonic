efd67b88574f712ce1c1e4ebb1daf6f1f8cd3d3b
Mike Medvedev <mmedvedev20@gmail.com>
2025-04-28 00:44:05 -0400

    added comment to profile page

--- src/app/(app)/(tabs)/profile/index.tsx
+++ src/app/(app)/(tabs)/profile/index.tsx
@@ -1,5 +1,6 @@
 import ProfileView from "@/features/Profile/Views/ProfileView";
 
+/** Render the Profile Page which internally gets current user profile */
 export default function Profile() {
   return <ProfileView />;
 }
